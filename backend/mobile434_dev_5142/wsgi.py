d55f2bd00101d3214078efeae79360f6719e324a
Crowdbotics <team@crowdbotics.com>
2020-05-28 07:54:29 +0000

    Initial commit.

---
+++ backend/mobile434_dev_5142/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mobile434_dev_5142 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mobile434_dev_5142.settings')
+
+application = get_wsgi_application()
